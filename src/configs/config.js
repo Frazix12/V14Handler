16a1134bb951c9b93ddd343d78be41796ed2ec4a
Frazix12 <71058095+Frazix12@users.noreply.github.com>
2022-04-03 23:53:19 +0600

    Fixed

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  token:
-    "ODk3Mzg1Mzg2Nzk0NDQ2ODc5.YWU5RA.UNXX9yvo0YmYRg21sANNnEvjjyA" ||
-    process.env.TOKEN,
+  token: "TOKEN_HERE" || process.env.TOKEN,
   Global: false,
 };
